ddaf26ab5bf40454db9426e4b35bb5f04addfcbc
RAMDANI ALI <ali.ramdani.dz@viacesi.fr>
2020-12-13 18:25:04 +0100

    socket c#

---
+++ Client/Client.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace Client
+{
+    class Client
+    {
+        static void Main(string[] args)
+        {
+            Socket mySender = SeConnecter();
+            Deconnecter(mySender);
+        }
+
+        private static Socket SeConnecter()
+        {
+            // Establish the remote endpoint  
+            // for the socket. This example  
+            // uses port 11111 on the local  
+            // computer. 
+            IPHostEntry ipHost = Dns.GetHostEntry(Dns.GetHostName());
+            IPAddress ipAddr = ipHost.AddressList[0];
+            IPEndPoint localEndPoint = new IPEndPoint(ipAddr, 11111);
+
+            // Creation TCP/IP Socket using  
+            // Socket Class Costructor 
+            Socket sender = new Socket(ipAddr.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+
+            try
+            {
+                // Connect Socket to the remote  
+                // endpoint using method Connect() 
+                sender.Connect(localEndPoint);
+
+                // We print EndPoint information  
+                // that we are connected 
+                Console.WriteLine("Socket connected to -> {0} ", sender.RemoteEndPoint.ToString());
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+
+            return sender; 
+        }
+
+        private static void Deconnecter (Socket socket)
+        {
+
+            try
+            {
+                // Creation of messagge that 
+                // we will send to Server 
+                byte[] messageSent = Encoding.ASCII.GetBytes("Test Client<EOF>");
+                int byteSent = socket.Send(messageSent);
+
+                // Data buffer 
+                byte[] messageReceived = new byte[1024];
+
+                // We receive the messagge using  
+                // the method Receive(). This  
+                // method returns number of bytes 
+                // received, that we'll use to  
+                // convert them to string 
+                int byteRecv = socket.Receive(messageReceived);
+                Console.WriteLine("Message from Server -> {0}", Encoding.ASCII.GetString(messageReceived, 0, byteRecv));
+
+                // Close Socket using  
+                // the method Close() 
+                socket.Shutdown(SocketShutdown.Both);
+                socket.Close();
+            }
+            catch (ArgumentNullException ane)
+            {
+                Console.WriteLine("ArgumentNullException : {0}", ane.ToString());
+
+            }
+            catch (SocketException se)
+            {
+                Console.WriteLine("SocketException : {0}", se.ToString());
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Unexpected exception : {0}", e.ToString());
+            }
+
+
+        }
+    }
+}
+
+
+
+
---
+++ ServerExia/Server.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace ServerExia
+{
+    class Server
+    {
+
+        static void Main(string[] args)
+        {
+            Socket Myserver = SeConnecter();
+            Socket ClientHandler = AccepterConnection(Myserver);
+            EcouterReseau(ClientHandler);
+            Deconnecter(ClientHandler);
+        }
+
+        private static Socket SeConnecter()
+        {
+            IPHostEntry ipHost = Dns.GetHostEntry(Dns.GetHostName());
+            IPAddress ipAddr = ipHost.AddressList[0];
+            IPEndPoint localEndPoint = new IPEndPoint(ipAddr, 11111);
+
+            Socket listener = new Socket(ipAddr.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+
+            try
+            {
+                listener.Bind(localEndPoint);
+                listener.Listen(10);
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+
+            return listener; 
+        }
+
+        private static Socket AccepterConnection(Socket socket)
+        {
+
+
+            Socket clientSocket = socket;
+
+            try
+            {
+                Console.WriteLine("Attente Connexion  ... ");
+                clientSocket.Accept();
+            }
+            catch (Exception e) 
+            {
+                Console.WriteLine(e.ToString());
+            }
+
+            return clientSocket; 
+
+        }
+
+
+        private static void EcouterReseau(Socket socket)
+        {
+            while (true)
+            {
+                byte[] bytes = new Byte[1024];
+                string data = null;
+
+                while (true)
+                {
+                    int numByte = socket.Receive(bytes);
+
+                    data += Encoding.ASCII.GetString(bytes, 0, numByte);
+
+                    if (data.IndexOf("<EOF>") > -1)
+                        break;
+                }
+
+                Console.WriteLine("Message Recu -> {0} ", data);
+
+            }
+        }
+
+
+        private static void Deconnecter(Socket socket)
+        {
+            byte[] message = Encoding.ASCII.GetBytes("Test Server");
+            socket.Send(message);
+            socket.Shutdown(SocketShutdown.Both);
+            socket.Close();
+        }
+
+
+    }
+}
